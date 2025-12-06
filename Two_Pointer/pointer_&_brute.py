a48252b5cd882ca55c42080d0530e49ccd333e91
Kadir Kacır <kadirk061942@gmail.com>
2025-12-06 14:05:54 +0300

    pointer_&_brute.py
    Refactor two-pointer and brute force functions for clarity and performance. Update target value for testing.

--- Two_Pointer/pointer_&_brute.py
+++ Two_Pointer/pointer_&_brute.py
@@ -1,49 +1,48 @@
 import random
 import time
 
+def brute_force_solution(arr,target):
+    
+    n=len(arr)
+    for i in range(n):
+        for j in range(i+1,n):
+            if arr[i]+arr[j]==target:
+                return True
+            
+    return False
 
-
-def two_pointer(target,arr):
-
-    left,right=0,len(arr)-1
-
+def two_pointer(arr,target):
+    n=len(arr)
+    arr.sort()
+    
+    left,right=0,n-1
+    
     while left<right:
-        current_sum=arr[left]+arr[right]
+        current=arr[left]+arr[right]
 
-        if current_sum==target:
+        if current ==target:
             return True
-        elif current_sum>target:
+    
+        elif current >target:
             right-=1
-        elif current_sum<target:
+        elif current <target:
             left+=1
     
     return False
 
-def brute_force_solution(target,arr):
-
-    n=len(arr)
-    for i in range(n):
-        for j in range(i+1,n):
-            curr_=arr[i]+arr[j]
-            
-            if curr==target:
-                return True
-    
-    return False
-
 new_arr=[random.randint(1,1000) for _ in range(10000)]
-target=1500
-new_arr.sort()
-
-# test start 
-start=time.time()
-two_pointer(target,new_arr)
-finish=time.time()-start
+target=5000  # en kötü seneryoyu düşünürsek asıl performans farklarını bulabiliriz
 
 start=time.time()
-brute_force_solution(target,new_arr)
-fin=time.time()-start
+brute_force_solution(new_arr,target)
+duration=time.time()-start
 
-print(f"two pointer: {finish:.10f}")
-print(f"brute force solution: {fin:.10f}")
 
+
+start=time.time()
+two_pointer(new_arr,target)
+duration1=time.time()-start
+
+print(f"brute force solution: {duration:.6f}")
+print(f"two pointer :  {duration1:.6f} ")
+print("-"*50)
